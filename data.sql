0007063cd4a98082a29efb3ef881b273cdba3be8
kayemba <kayembejonathan5@gmail.com>
2023-06-12 20:01:27 +0200

    Create a database

--- data.sql
+++ data.sql
@@ -1,3 +1,7 @@
-CREATE TABLE animals (
-
-);
+INSERT INTO animals (name) VALUES ('Jackie');
+INSERT INTO animals (name) VALUES ('Rocky');
+INSERT INTO animals (name) VALUES ('Champlin');
+INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Agumon','2020-02-03', 1.20, true, 0);
+INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Gabumon','2018-11-05', 4, true, 2);
+INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Pikachu','2021-01-07', 10.05, false, 1);
+INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts) VALUES ('Devimon','2017-05-12', 12, true, 3);
---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE animals (
+id: integer
+name: string
+date_of_birth: date
+escape_attempts: integer
+neutered: boolean
+weight_kg: integer
+);
