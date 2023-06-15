3af447be416541eb5e34bc3830a2f31bea90d17b
kayemba <kayembejonathan5@gmail.com>
2023-06-15 09:20:25 +0200

    update the app

--- data.sql
+++ data.sql
@@ -4,10 +4,10 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg)
         (3,'Pikachu', date '2021-01-07', 1, false, 15.04), 
         (4,'Devimon', date '2017-05-12', 5, true, 11.00);
         
-INSERT INTO animals VALUES (5,'Charmander', '2020-02-08', 0, false, -11),
+INSERT INTO animals VALUES (5,'Charmander', '2020-02-08', 0, false, -11), 
                            (6,'Plantmon', '2021-11-15', 2 , true , -5.7),
                            (7,'Squirtle', '1993-04-02', 3 , false , -12.13),
                            (8, 'Angemon', '2005-06-12', 1 , true , -45),
                            (9,'Boarmon',  '2005-06-07', 7, true , 20.4),
                            (10,'Blossom', '1998-10-13', 3 , true , 17),
-                           (11,'Ditto', '2022-05-14' , 4 , true , 22); 
+INSERT INTO animals VALUES (12,'Ditto', '2022-05-14' , 4 , true , 22);
